2682e814ee745e1b5f6c96ba0d839ee68bc33ced
Noah <noahyasskin@gmail.com>
2016-10-26 17:59:50 -0700

    add some notes for npm basics

---
+++ javascript_track/npm_basics.js
@@ -0,0 +1,74 @@
+Introducing npm 5:52
+with Andrew Chalkley
+In this video, we'll take a look at what npm is and what it can do for you.
+
+Site
+npm Package Examples
+
+Express
+Passport
+jQuery Plugins
+    tipso
+Grunt
+gulp.js
+CoffeeScript
+TypeScript
+Sass
+Cordova
+PhoneGap
+Ionic
+johnny-five
+Espruino
+Tessel
+
+Node.js and npm Install Guides
+Mac
+Windows
+Linux
+
+npm: a javascript package manager
+
+packages are bundles of software
+npm packages are mainly JavaScript
+express web framework
+passport authentication framework
+
+npm does not stand for anything
+
+    How to Find and Choose Packages 5:17
+with Andrew Chalkley
+In this video, we will take a look at the different kind of methods you can use to find an npm package or Node.js module.
+
+There are a number of indicators that you can use to determine which package to use.
+
+    Popularity on npm – the number of downloads
+Release Date – the more recent the better
+Number of Releases – the more frequent the better
+Passing Tests - if there's tests passing
+Number of Open Issues – the less the better
+Popularity on GitHub – The more Stars, Forks and Watches the better
+Number of Contributors on GitHub – more eyes on the code the better
+
+with Andrew Chalkley
+There are two ways to install an npm package. In this video we will take a look at the types of packages you would include in a project you are coding.
+
+Clear the screen
+clear
+npm Command Line Usage
+
+See list of commands
+
+npm
+Get help for a command
+
+npm <command> -h
+e.g. npm install -h
+Installing a package
+
+npm install <package_name>
+e.g. npm install bcrypt
+e.g. with a flag npm install bcrypt --python=mySpecialPythonExecutable
+Documentation
+
+require()
+bcrypt
