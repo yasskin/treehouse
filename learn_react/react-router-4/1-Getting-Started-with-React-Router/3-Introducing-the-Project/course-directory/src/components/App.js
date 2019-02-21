9b24d728215be85334d4bf50150fe63e286fb4d5
Noah Yasskin <noahyasskin@gmail.com>
2019-02-20 20:40:52 -0800

    add react-router coursework

--- learn_react/react-router-4/1-Getting-Started-with-React-Router/3-Introducing-the-Project/course-directory/src/components/App.js
+++ learn_react/react-router-4/1-Getting-Started-with-React-Router/3-Introducing-the-Project/course-directory/src/components/App.js
@@ -1,9 +1,26 @@
 import React from 'react';
+import {
+  BrowserRouter,
+  Route
+} from 'react-router-dom';
+
+// App component
+import Header from './Header';
+import Home from './Home';
+import About from './About';
+import Teachers from './Teachers';
+import Courses from './Courses';
 
 const App = () => (
-  <div className="container">
-
-  </div>
+  <BrowserRouter>
+    <div className="container">
+    <Header />
+      <Route exact path="/" component={Home} />
+      <Route path="/about" render={ () => <About title='About' /> } />
+      <Route path="/teachers" component={Teachers} />
+      <Route path="/courses" componet={Courses} />
+    </div>
+  </BrowserRouter>
 );
 
 export default App;
